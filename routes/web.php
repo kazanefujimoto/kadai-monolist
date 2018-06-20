50e25a7293ddd1d934657e22dd6cb579af99ebf3
kazane.fujimoto <kazane.fujimoto@rakuten.com>
2018-06-20 02:01:26 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('/', 'WelcomeController@index');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
